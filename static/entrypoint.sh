fa7d4701e5f26f1ebd3febc669073bb0c0fa0976
Bob D'Errico <bob@bobderrico.com>
2017-01-18 22:01:56 -0500

    Creates yadaguru/static version 1.2. Updates qa docker compose file to use 1.2

--- static/entrypoint.sh
+++ static/entrypoint.sh
@@ -2,8 +2,8 @@
 
 cd /srv/yadaguru/yadaguru-app
 echo "(yadaguru-app) pulling the latest updates and switching to: [$MY_BRANCH]"
-git pull && git checkout $MY_BRANCH
+git pull && git checkout $MY_BRANCH && npm install && bower install --force-latest --allow-root && gulp build
 
 cd /srv/yadaguru/yadaguru-admin
 echo "(yadaguru-admin) pulling the latest updates and switching to: [$MY_BRANCH]"
-git pull && git checkout $MY_BRANCH
+git pull && git checkout $MY_BRANCH && npm install --unsafe-perm
