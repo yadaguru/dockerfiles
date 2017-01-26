c20d3ae80664d201cb7d117f88db72ac68bc1cb3
Bob D'Errico <bob@bobderrico.com>
2017-01-25 22:16:35 -0500

    Create QA-M stack

--- static/entrypoint.sh
+++ static/entrypoint.sh
@@ -3,7 +3,7 @@
 cd /srv/yadaguru/yadaguru-app
 echo "(yadaguru-app) pulling the latest updates and switching to: [$MY_BRANCH]"
 git pull && git checkout $MY_BRANCH && npm install && bower install --force-latest --allow-root && \
-gulp build && gulp constants --db qa
+gulp build && gulp constants --db $NODE_ENV
 
 cd /srv/yadaguru/yadaguru-admin
 echo "(yadaguru-admin) pulling the latest updates and switching to: [$MY_BRANCH]"
