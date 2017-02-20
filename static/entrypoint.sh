1a223c298bf9f7833467ba41f087e2851e7ba743
Bob D'Errico <bob@bobderrico.com>
2017-02-20 00:32:51 -0500

    Updates static containers to use correct NODE_ENV version and bower resolutions

--- static/entrypoint.sh
+++ static/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 cd /srv/yadaguru/yadaguru-app
-echo "(yadaguru-app) pulling the latest updates and switching to: [$MY_BRANCH]"
+git checkout -- .
+echo "[yadaguru-app] pulling the latest updates and switching to: [$MY_BRANCH]"
 git pull && git checkout $MY_BRANCH && npm install && bower install --force-latest --allow-root && \
+echo "[yadaguru-app] for $NODE_ENV"
 gulp build && gulp constants --db $NODE_ENV
 
 cd /srv/yadaguru/yadaguru-admin
-echo "(yadaguru-admin) pulling the latest updates and switching to: [$MY_BRANCH]"
+git checkout -- .
+echo "[yadaguru-admin] pulling the latest updates and switching to: [$MY_BRANCH]"
 git pull && git checkout $MY_BRANCH && npm install --unsafe-perm
 
 nginx
