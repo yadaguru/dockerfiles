163f676a6bf9df1fe844b752e1ceff1bbe0b8631
Bob D'Errico <bob@bobderrico.com>
2017-05-06 00:06:44 -0400

    Updates prod docker-compose files to work with new images

--- compose/yada-dev-stack/entrypoint-static.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Starting nginx for local development"
-nginx
--- compose/yada-test-stack-m/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /opt/yadaguru-api
-npm install
-
-echo "Starting Yadaguru API in development mode"
-nodemon --debug ./index.js
